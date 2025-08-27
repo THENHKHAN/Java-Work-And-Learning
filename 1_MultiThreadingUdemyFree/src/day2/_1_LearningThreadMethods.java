7fe935002adddf59af4fb6f3bbe034f28b67d026
Noorul <noorulhuda101099@gmail.com>
2025-08-28 01:58:45 +0530

    27Aug2025 some basic methods learned

--- 1_MultiThreadingUdemyFree/src/day2/_1_LearningThreadMethods.java
+++ 1_MultiThreadingUdemyFree/src/day2/_1_LearningThreadMethods.java
@@ -4,12 +4,27 @@ import day2.threadmethods.ThreadMethodCollections;
 
 public class _1_LearningThreadMethods {
     public static void main(String[] args) {
-        System.out.println("-------- Starting Thread Methods and there working ---------");
+        System.out.println("\n\n-------- Starting Thread Methods and there working ---------\n");
 
-        ThreadMethodCollections th = new ThreadMethodCollections(); // thread created.
-//        th.start();
-        System.out.println("Current thread isAlive -1 : " + th.isAlive());
-        System.out.println("Current thread name : " + ThreadMethodCollections.getMyThreadName(th));
-        System.out.println("Current thread isAlive -2: " + th.isAlive());
+        System.out.println("lets print current Thread name - i.e. check main thread created by JVM: " + Thread.currentThread().getName());
+                         // lets print current Thread name - i.e. check main thread created by JVM: main
+        //  lets change the main thread name :
+        Thread.currentThread().setName("Noor Thread");
+        System.out.println("After resetting the main thread AFTER Changing Default main THREAD: " + Thread.currentThread().getName()); //
+        // After resetting the main thread AFTER Changing Default main THREAD: Noor Thread
+
+
+
+
+//        System.out.println("inifinite : " + 10/0);
+        /* See exception  name got change
+        Exception in thread "Noor Thread" java.lang.ArithmeticException: / by zero
+	at day2._1_LearningThreadMethods.main(_1_LearningThreadMethods.java:16)
+         */
+
+        System.out.println("\n-------------      Let's Learn other methods of Thread Class : getName(), setName(), isAlive(), currentThread() methods    --------------------------------   \n");
+
+//            System.exit(1);
+            ThreadMethodCollections.myMain();
     }
 }
---
+++ 1_MultiThreadingUdemyFree/src/day2/threadmethods/thread/management/and/control/DaemonThread.java
@@ -0,0 +1,37 @@
+package day2.threadmethods.thread.management.and.control;
+
+// Done - In the ThreadMethodCollections class we covered basic method and their working now:
+/**
+ https://www.digitalocean.com/community/tutorials/multithreading-in-java#creating-threads-in-java
+
+        ------- Thread Management and Control  OR Prevent Thread execution Methods ---------------
+
+ Once you’ve created threads in Java, you need ways to control how and when they execute.
+    Java provides several built-in methods in the Thread class to manage thread behavior effectively.
+
+ We’ll cover the following:
+
+ Starting a thread   --- already done
+ Putting a thread to sleep
+ Waiting for a thread to finish
+ Yielding execution
+ Setting priorities
+ Creating daemon threads
+ Stopping threads (the right way)
+
+ **/
+
+
+
+
+
+public class DaemonThread {
+
+//    https://www.youtube.com/watch?v=qfyd0BsqEJ0&list=PLlhM4lkb2sEh1pBs0KBb63PlKKqRx6M1a&index=8                      smart Programming
+
+//durgesh -TS- 00:53:00 ---->  https://www.youtube.com/watch?v=4uQAokzfm-Q&t=745s          with  Producer–Consumer Problem Mini Project
+
+    public static void myMain(){
+        System.out.println("\n--------- This is Daemon Thread Class : VVV Important for interview perspective as well --------- \n");
+    }
+}
